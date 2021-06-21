5696a036c588a48943862f0c9b0c027c80cba91a
Keu-ni <skcor8909@gmail.com>
2021-06-22 01:44:05 +0900

    화면 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const response = await fetchWeather(query);
+      setWeather(response);
+      setQuery('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Search...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div>
+          <h2>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].main}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
