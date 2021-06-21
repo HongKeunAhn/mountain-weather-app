070dac96596d8fd8be84098365e03ca7bdb174ff
Keu-ni <skcor8909@gmail.com>
2021-06-22 08:26:58 +0900

    glass morphism 스타일 적용

--- src/App.js
+++ src/App.js
@@ -15,33 +15,39 @@ function App() {
   };
 
   return (
-    <div>
-      <input
-        type='text'
-        placeholder='Search...'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
-      {weather.main && (
-        <div>
-          <h2>
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div>
-            {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
-          </div>
-          <div>
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].main}
-            />
-            <p>{weather.weather[0].description}</p>
-          </div>
+    <div className='container'>
+      <main className='main'>
+        <div className='search-input-wrap'>
+          <input
+            type='text'
+            className='search-input'
+            placeholder='Search...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={search}
+          />
         </div>
-      )}
+        {weather.main && (
+          <div className='weather-card'>
+            <h2 className='weather-city'>
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </h2>
+            <div className='weather-temperature'>
+              <span>{Math.round(weather.main.temp)}</span>
+              <sup>&deg;C</sup>
+            </div>
+            <div className='weather-viewer'>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                className='weather-icon'
+                alt={weather.weather[0].main}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
