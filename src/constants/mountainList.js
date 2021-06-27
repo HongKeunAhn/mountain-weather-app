3a67e17667a8952d2fc6e5321dec9ad073a999d2
Keu-ni <skcor8909@gmail.com>
2021-06-27 17:24:01 +0900

    <feat> 국립공원 산 목록 추가

---
+++ src/constants/mountainList.js
@@ -0,0 +1,70 @@
+export const mountainList = {
+  한라산: {
+    latitude: 33.36,
+    longitude: 126.53,
+  },
+  지리산: {
+    latitude: 35.34,
+    longitude: 127.73,
+  },
+  설악산: {
+    latitude: 38.12,
+    longitude: 128.47,
+  },
+  덕유산: {
+    latitude: 35.86,
+    longitude: 127.75,
+  },
+  오대산: {
+    latitude: 37.84,
+    longitude: 128.73,
+  },
+  치악산: {
+    latitude: 37.36,
+    longitude: 128.06,
+  },
+  태백산: {
+    latitude: 37.12,
+    longitude: 128.92,
+  },
+  계룡산: {
+    latitude: 36.34,
+    longitude: 127.21,
+  },
+  소백산: {
+    latitude: 36.96,
+    longitude: 128.48,
+  },
+  월악산: {
+    latitude: 36.89,
+    longitude: 128.11,
+  },
+  속리산: {
+    latitude: 36.54,
+    longitude: 127.87,
+  },
+  내장산: {
+    latitude: 35.47,
+    longitude: 126.89,
+  },
+  무등산: {
+    latitude: 35.13,
+    longitude: 126.99,
+  },
+  월출산: {
+    latitude: 34.77,
+    longitude: 126.7,
+  },
+  가야산: {
+    latitude: 35.82,
+    longitude: 128.11,
+  },
+  주왕산: {
+    latitude: 36.39,
+    longitude: 129.17,
+  },
+  북한산: {
+    latitude: 37.66,
+    longitude: 126.98,
+  },
+};
