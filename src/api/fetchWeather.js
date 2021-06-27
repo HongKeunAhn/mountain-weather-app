7cbb3827f098dccbb98309a334ba228fa8579f8f
Keu-ni <skcor8909@gmail.com>
2021-06-27 17:23:11 +0900

    <feat> api 추가 (위도, 경도 입력받는 api)

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -15,3 +15,23 @@ export const findCityWeather = async (cityName) => {
 
   return data;
 };
+
+export const findMountainWeather = async (offset = {}) => {
+  const { latitude, longitude } = offset;
+
+  try {
+    const response =  await axios.get(URL, {
+      params: {
+        lat: latitude,
+        lon: longitude,
+        APPID: API_KEY,
+        units: 'metric',
+        lang: 'kr',
+      },
+    });
+    
+    return response
+  } catch (error) {
+    return error.response;
+  }
+}
